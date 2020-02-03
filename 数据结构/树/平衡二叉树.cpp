c84e66d4a2f174ed7ec8c9c0923d3c2509977e93
muhenan <1149423577@qq.com>
2020-02-03 17:52:49 +0800

    2020-2-3

---
+++ 数据结构/树/平衡二叉树.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+#include<algorithm>
+#include<vector>
+#include<map>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
